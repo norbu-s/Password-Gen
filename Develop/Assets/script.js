6fab34391128bd591e2891fcb1f86b44a420132d
Norbu Sherpa <norbuts@hotmail.com>
2020-11-30 02:13:47 +1100

    Updated page css, button,alert added

--- Develop/Assets/script.js
+++ Develop/Assets/script.js
@@ -1,4 +1,37 @@
-// Assignment Code
+function ping() {
+  
+  
+  var txt;
+  
+  if (confirm("Do you want to create a new password?")) 
+  var userinput = prompt ("Enter a password length.")
+}
+  
+ 
+
+
+/*if 
+var pass.length = function stringlength(inputtxt, minlength, maxlength)
+{ 
+var field = inputtxt.value; 
+var mnlen = minlength;
+var mxlen = maxlength;
+
+if(field.length<mnlen || field.length> mxlen)
+{ 
+alert("Choose a lenght between " +mnlen+ " and " +mxlen+ " characters");
+return false;
+}
+else
+{ 
+alert('Password length has been accepted.');
+return true;
+})
+
+prompt ("Do you want the password to be upper case ot lower case")
+}
+if ( )
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -12,3 +45,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+</script>*/
