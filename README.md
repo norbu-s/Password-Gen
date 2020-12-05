1ddfa40b24e0b6a76309c69441b603dd94088cca
Norbu Sherpa <norbuts@hotmail.com>
2020-12-06 02:45:01 +1100

    Final changes

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Password-Gen
+
+# Project Summary
+Simple app for genereationrandom password
+
+# Project Goals
+Create an app to generate a random password based on the criteria set by the user.
+
+# Installation
+N/A
+
+# Author 
+Name: Norbu Sherpa
+
+# Links for Resource
+
+* https://developer.mozilla.org/en-US/docs/Web/HTML/Element/aside
+
+* https://www.w3schools.com/
+
+* https://www.youtube.com/watch?v=duNmhKgtcsI&feature=youtu.be
+
+* https://www.florin-pop.com/blog/2019/09/100-days-100-projects/
+
+
+# Github Repository:
+* https://github.com/norbu-s/Password-Gen
+
+# Project URL:
+
+* 
+
+# Application Screenshot:
+
+[Alt screenshot ] (Develop/Images/Application Screenshot.png)
--- script.js
+++ script.js
@@ -1,82 +1,78 @@
-var generateBtn = document.querySelector("#generate");
-var lengthEl = "length";
-var lowerCaseEl = "abcdefghijklmnopqrstuvwxyz";
-var upperCaseEl = "ABCDEFGHIJKLMNOPQRSTUVWXY";
-var numberEl = "0123456789";
-var specialCharactersEl = "~!@#$%^&*()_+=-{}|[]\<>?/"; 
+var lengthmin = 8;
+var lengthmax = 128;
+var generatedPassword = "";
+var generateEl = document.getElementById('generate');
+var resultEl = document.getElementById('result');
+
+var randomFunc = {
+	lower: getRandomLowerCase,
+	upper: getRandomUpperCase,
+	number: getRandomNumber,
+  symbol: getRandomSymbol
+}
 
 function generateLength() {
-  let inputValue = prompt("Choose a length of at least 8 characters and no more than 128 characters");
-  let length = parseInt(inputValue);
-  if (length >= 8 && length <= 128){
-    generatePassword(inputValue)
+    var inputValue = prompt("Choose a length of at least 8 characters and no more than 128 characters");
+    var length = parseInt(inputValue)
+    if (length >= lengthmin && lengthmax <= 128){
+      generatePassword()
+    }
+    else{ alert("Not a valid Input")
+    generateLength()
   }
-  else{ alert("Not a valid Input")
-  generateLength()
- }
+}
+
+for(let i=0; i<length; i++) {
+  typesArr.forEach(type => {
+  var funcName = Object.keys(type)[0];
+  generatedPassword += randomFunc[funcName]();
+  });
 }
 
 function generatePassword(){
-  
-    let lowerCase = confirm("Do you want the password to include a Lowercase?");
-    if (lowerCase) true;{
+      var lowerCaseinput = confirm("Do you want the password to include a Lowercase?");
+      if (lowerCaseinput) true;{
       getRandomLowerCase()
-      console.log(lowerCase)
-    } 
+  } 
+      var upperCaseinput = confirm("Do you want the password to include a Uppercase?");
+      if  (upperCaseinput) true;{
+      getRandomUpperCase()
+  }
+      var numberInput = confirm ("Do you want the password to include a Number?");
+      if (numberInput) true;{
+      getRandomNumber()
+  }
+      var specialCharacterinput = confirm("Do you want the password to include a Symbol?");
+      if (specialCharacterinput) true;{
+      getRandomSymbol()
+  }
+};
 
-    let upperCase = confirm("Do you want the password to include a Uppercase?")
-    if  (upperCase) true;{
-      getRandomUpperCase(upperCase)
-      console.log()
-    }
-
-    let number = confirm ("Do you want the password to include a number?")
-    if (number) true;{
-      getRandomNumber(number)
-    }
+  function getRandomLowerCase() {
+      return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  }
+    
+  function getRandomUpperCase() {
+      return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
   }
 
-    let specialCharacter = confirm("Do you want the password to include a number?")
-    if (specialCharacter) true;{
-      getRandomSymbol(specialCharacter)
-    }
-  
-    function getRandomLowerCase() {
-      return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-      console.log(getRandomLowerCase)
-      }
-    
-
-    function getRandomUpperCase() {
-      return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-      }
-
-    function getRandomNumber() {
+  function getRandomNumber() {
       return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-      console.log(getRandomNumber)
-      }
+  }
       
-
-    function getRandomSymbol () {
-      let inputValue = prompt("Do you want to include a special Character");
-      const symbols = "~!@#$%^&*()_+=-{}|[]\<>?/";
-      return symbols[Math.floor(Math.random() * symbols.length)];
-      console.log(getRandomSymbol)
+  function getRandomSymbol(){
+    specialCharacterinput[(Math.floor(Math.random() * specialCharacterinput.length))];
+      return specialCharacterinput[(Math.floor(Math.random()*specialCharacterinput.length))];
     }
 
-//DOM elements
-var pw = document.getElementById('pw');
-var length = document.getElementById('length');
-var uppercase = document.getElementById('uppercase');
-var lowercase = document.getElementById('lowercase');
-var number = document.getElementById('number');
-var symbol = document.getElementById('symbol');
-var generate = document.getElementById('generate');
+  generate.addEventListener('click', () => {
+    resultEl.textarea = generatePassword();
+  });
+  
+  writePassword()
 
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
+  function writePassword() {
+      var password = generatePassword();
+      var passwordText = document.querySelector("#password");
+      passwordText.value = password;
+  }
